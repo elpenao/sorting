78926aa5cd1fd549e0663c1c81b8153de0a4b692
elpenao <elpenao@gmail.com>
2016-01-15 11:06:43 -0500

    with solutions

--- bubblesort.js
+++ bubblesort.js
@@ -13,6 +13,28 @@ function bubbleSort(arr, marker){
 	marker--;
 	if (swaps > 0) return bubbleSort(arr, marker);
 	return arr;
+	// var sorted = false;
+ //  for (var end = arr.length; end > 0 && !sorted; end--) { // passes
+ //    sorted = true; // assume until proven incorrect
+ //    for (var j = 0; j < end; j++) { // bubbling
+ //      if (!inOrder(arr, j)) {
+ //        swap(arr, j);
+ //        sorted = false;
+ //      }
+ //    }
+ //  }
+ //  return arr;
+}
+
+function inOrder (array, index) { // pure function
+  if (index === array.length - 1) return true;
+  return array[index] < array[index + 1];
+}
+
+function swap (array, index) { // side effects
+  var oldLeftValue = array[index];
+  array[index] = array[index + 1];
+  array[index + 1] = oldLeftValue;
 }
 
 
--- mergesort.js
+++ mergesort.js
@@ -1,36 +1,64 @@
 function mergeSort(arr) {
 	if (arr.length <= 1) return arr;
-	var half = Math.ceil(arr.length / 2);
-	var left = arr.slice(0,half);
-	var right = arr.slice(half);
+	var splits = split(arr),
+	    left = splits[0],
+	    right = splits[1];
+	 return merge(mergeSort(left), mergeSort(right));
+	// var half = arr.length / 2;
+	// var left = arr.slice(0,half);
+	// var right = arr.slice(half);
 	
-	left = mergeSort(left);
-	right = mergeSort(right);
-	return merge(left, right);
+	// left = mergeSort(left);
+	// right = mergeSort(right);
+	// return merge(left, right);
+}
+
+
+function split (array) {
+  var center = array.length / 2;
+  var left = array.slice(0, center);
+  var right = array.slice(center);
+  return [left, right];
 }
 
 function merge(left, right){
-	var merged = [];
-	function addEl(left, right) {
-        if (left[0] <= right[0]) {
-			merged.push(left[0]);
-			var lshift = left.slice(1);
-			if (lshift.length > 0) {
-                addEl(lshift,right);
-            } else {
-                merged = merged.concat(right);
-            }
-		} else {
-			merged.push(right[0]);
-            var rshift = right.slice(1);
-            if (rshift.length > 0) {
-                addEl(left,rshift);
-            } else {
-               merged = merged.concat(left);
-            }
-		}
+	var merged = [],
+      leftIdx = 0,
+      rightIdx = 0,
+      leftVal,
+      rightVal;
+  // admittedly pretty convoluted; but we do this in order to avoid shift.
+  while (leftIdx < left.length || rightIdx < right.length) {
+    leftVal = left[leftIdx];
+    rightVal = right[rightIdx];
+    if (leftVal < rightVal || rightVal === undefined) {
+      merged.push(leftVal);
+      leftIdx++;
+    } else {
+      merged.push(rightVal);
+      rightIdx++;
     }
-    addEl(left,right);
+  }
+	// function addEl(left, right) {
+ //        if (left[0] <= right[0]) {
+	// 		merged.push(left[0]);
+	// 		var lshift = left.slice(1);
+	// 		if (lshift.length > 0) {
+ //                addEl(lshift,right);
+ //            } else {
+ //                merged = merged.concat(right);
+ //            }
+	// 	} else {
+	// 		merged.push(right[0]);
+ //            var rshift = right.slice(1);
+ //            if (rshift.length > 0) {
+ //                addEl(left,rshift);
+ //            } else {
+ //               merged = merged.concat(left);
+ //            }
+	// 	}
+ //    }
+ //    addEl(left,right);
 	return merged;
 }
 
